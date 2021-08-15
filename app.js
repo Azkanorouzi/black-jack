39740b6fef1f2f7325d391c3586ce79a24be8881
Azka <a.azkanorouzi@gmail.com>
2021-08-15 11:03:39 +0430

    completed

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-let count = 0;
-let a = document.getElementById("count-el");
+let a = 0;
+let b = document.getElementById("count-el");
+let s = document.getElementById("previous-el");
 function increment(){
- count += 1;
- a.innerText=count;
+ a+=1;
+ b.textContent=a;
 }
+function save() {
+let countstr = a + " - ";
+s.textContent += countstr;
+a = 0;
+b.textContent = 0;
+}
+
+
+
+
