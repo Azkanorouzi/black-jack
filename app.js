fac495c84e294a3fa0181480211ed3ce060ddee7
Azka <a.azkanorouzi@gmail.com>
2021-08-16 12:42:15 +0430

    first step

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-let firstCard=10;
-let secondCard=10;
+let firstCard = 5;
+let secondCard = 11;
+let cards = [firstCard,secondCard] //array - order list of items
 let sum = firstCard + secondCard;
 let isAlive = true;
-let hasBlackjack =false;
+let hasBlackjack = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
-
 let sumEl = document.querySelector(".sum-el");
-let cards = document.querySelector("#card-el");
+let cardEl = document.querySelector("#card-el");
 
-function startgame(){
-  if (sum<=20){
-   message= "do you want to draw a new card ?"
+function renderGame() {
+  if (sum <= 20) {
+    message = "do you want to draw a new card ?"
   }
-  else if (sum===21){
-   message = "you've got the blackjack!";
-   hasBlackjack=true;
+  else if (sum === 21) {
+    message = "you've got the blackjack!";
+    hasBlackjack = true;
   }
   else {
-   message = "you're out of the game !";
-   isAlive=false;
+    message = "you're out of the game !";
+    isAlive = false;
   }
- var cardss = " " + firstCard + " " + secondCard;
-  messageEl.textContent=message;
- sumEl.innerText = "Sum : "+" " + sum;
- cards.textContent ="Cards:" + " " + cardss;
+  messageEl.textContent = message;
+  sumEl.innerText = "Sum : " + " " + sum;
+  cardEl.textContent = "Cards:" + " " + cards[0] + " " + cards[1] ;
 }
+function startGame(){
+  renderGame();
+}
+function newCard() {
+  var thirdCard = 5;
+  cards.push(thirdCard)
+  console.log(cards)
+  sum += thirdCard;
+  renderGame();
+}
+
+
